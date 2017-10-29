35bc30de21a698bed302cc685fa49ea5e640e1bf
zurfyx <zurfyx@gmail.com>
2017-10-29 20:38:12 +0100

    maven web app

---
+++ src/main/servlets/FirstServlet.java
@@ -0,0 +1,25 @@
+package main.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "FirstServlet", urlPatterns = "/first")
+public class FirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String json = "{ hello: \"maven\"}";
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print(json);
+        out.flush();
+    }
+}
