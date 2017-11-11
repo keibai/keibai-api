92bfc841cf8b4af56b1c4fa218953ed0d01550de
hzhu <zmark2013@gmail.com>
2017-11-11 14:04:11 +0100

    Auction Model

--- src/main/java/models/Auction.java
+++ src/main/java/models/Auction.java
@@ -1,4 +1,90 @@
 package main.java.models;
 
+import java.util.Calendar;
+
 public class Auction {
+
+    private int id;
+    private String name;
+    private double starting_price;
+    private Calendar start_time;
+    private boolean is_valid;
+
+    private Event event;
+    private Owner owner;
+    private Status status;
+    private Winner winner;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getStarting_price() {
+        return starting_price;
+    }
+
+    public void setStarting_price(double starting_price) {
+        this.starting_price = starting_price;
+    }
+
+    public Calendar getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Calendar start_time) {
+        this.start_time = start_time;
+    }
+
+    public boolean isIs_valid() {
+        return is_valid;
+    }
+
+    public void setIs_valid(boolean is_valid) {
+        this.is_valid = is_valid;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Winner getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Winner winner) {
+        this.winner = winner;
+    }
 }
--- src/main/java/models/AuctionStatus.java
+++ src/main/java/models/AuctionStatus.java
@@ -1,4 +1,5 @@
 package main.java.models;
 
 public class AuctionStatus {
+
 }
