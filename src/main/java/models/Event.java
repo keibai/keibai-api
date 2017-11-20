974dbee8720266494e1c8f4e9a420b57ff9c460c
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-20 18:09:42 +0100

    Simplification of Event model

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -3,85 +3,13 @@ package main.java.models;
 import java.util.Calendar;
 
 public class Event {
-    private int id;
-    private String name;
-    private int auctionTime;
-    private String location;
-    private Calendar createdAt;
-    private Calendar updatedAt;
-    private String auctionType;
-    private String category;
-    private User owner;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAuctionTime() {
-        return auctionTime;
-    }
-
-    public void setAuctionTime(int auctionTime) {
-        this.auctionTime = auctionTime;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Calendar getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Calendar createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Calendar getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Calendar updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getAuctionType() {
-        return auctionType;
-    }
-
-    public void setAuctionType(String auctionType) {
-        this.auctionType = auctionType;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
+    public int id;
+    public String name;
+    public int auctionTime;
+    public String location;
+    public Calendar createdAt;
+    public Calendar updatedAt;
+    public String auctionType;
+    public String category;
+    public int ownerId;
 }
