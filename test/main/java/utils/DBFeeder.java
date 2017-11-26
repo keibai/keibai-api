05c6fd198364ad16e38b458f720300c284501bcc
Eduard Torres <etorres404@gmail.com>
2017-11-26 12:31:34 +0100

    Implementation of GoodSearch servlet

--- test/main/java/utils/DBFeeder.java
+++ test/main/java/utils/DBFeeder.java
@@ -1,14 +1,13 @@
 package main.java.utils;
 
-import main.java.dao.AuctionDAO;
-import main.java.dao.DAOException;
-import main.java.dao.EventDAO;
-import main.java.dao.UserDAO;
+import main.java.dao.*;
 import main.java.dao.sql.AuctionDAOSQL;
 import main.java.dao.sql.EventDAOSQL;
+import main.java.dao.sql.GoodDAOSQL;
 import main.java.dao.sql.UserDAOSQL;
 import main.java.models.Auction;
 import main.java.models.Event;
+import main.java.models.Good;
 import main.java.models.User;
 
 public class DBFeeder {
@@ -49,5 +48,16 @@ public class DBFeeder {
         AuctionDAO auctionDAO = AuctionDAOSQL.getInstance();
         Auction auction = auctionDAO.create(dummyAuction);
         return auction;
+    }
+
+    public static Good createDummyGood() throws DAOException {
+        Auction dummyAuction = createDummyAuction();
+
+        Good dummyGood = DummyGenerator.getDummyGood();
+        dummyGood.auctionId = dummyAuction.id;
+
+        GoodDAO goodDAO = GoodDAOSQL.getInstance();
+        Good good = goodDAO.create(dummyGood);
+        return good;
     }
 }
