49ab341731288bdbaba7c3cb13a88ce5b7ec402c
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-27 18:27:54 +0100

    AuctionID and OwnerID check in BidNew servlet

--- src/main/java/servlets/bid/BidNew.java
+++ src/main/java/servlets/bid/BidNew.java
@@ -2,10 +2,16 @@ package main.java.servlets.bid;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import main.java.dao.AuctionDAO;
 import main.java.dao.BidDAO;
 import main.java.dao.DAOException;
+import main.java.dao.UserDAO;
+import main.java.dao.sql.AuctionDAOSQL;
 import main.java.dao.sql.BidDAOSQL;
+import main.java.dao.sql.UserDAOSQL;
+import main.java.models.Auction;
 import main.java.models.Bid;
+import main.java.models.User;
 import main.java.utils.HttpRequest;
 import main.java.utils.HttpSession;
 import main.java.utils.JsonResponse;
@@ -23,12 +29,16 @@ public class BidNew extends HttpServlet {
 
     public static final String INVALID_AMOUNT_ERROR = "Invalid amount";
     public static final String AUCTION_ID_ERROR = "Missing auction ID";
+    public static final String AUCTION_NOT_EXIST_ERROR = "Auction does not exists";
+    public static final String OWNER_NOT_EXIST_ERROR = "Owner does not exist";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         JsonResponse jsonResponse = new JsonResponse(response);
         HttpSession session = new HttpSession(request);
         BidDAO bidDAO = BidDAOSQL.getInstance();
+        AuctionDAO auctionDAO = AuctionDAOSQL.getInstance();
+        UserDAO userDAO = UserDAOSQL.getInstance();
 
         int userId = session.userId();
         if (userId == -1) {
@@ -56,6 +66,33 @@ public class BidNew extends HttpServlet {
 
         if (unsafeBid.auctionId == 0) {
             jsonResponse.error(AUCTION_ID_ERROR);
+            return;
+        }
+
+        Auction auction;
+        try {
+            auction = auctionDAO.getById(unsafeBid.auctionId);
+        } catch (DAOException e) {
+            Logger.error("Get auction by ID " + unsafeBid.auctionId, e.toString());
+            jsonResponse.internalServerError();
+            return;
+        }
+        if (auction == null) {
+            jsonResponse.error(AUCTION_NOT_EXIST_ERROR);
+            return;
+        }
+
+        User owner;
+        try {
+            owner = userDAO.getById(unsafeBid.ownerId);
+        } catch (DAOException e) {
+            Logger.error("Get user by ID " + unsafeBid.ownerId, e.toString());
+            jsonResponse.internalServerError();
+            return;
+        }
+
+        if (owner == null) {
+            jsonResponse.error(OWNER_NOT_EXIST_ERROR);
             return;
         }
 
