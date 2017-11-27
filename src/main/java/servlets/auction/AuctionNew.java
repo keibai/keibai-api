8eb47156b1f233b696b24a34d33b67359b7b6c94
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-27 18:22:45 +0100

    OwnerID and EventID check in AuctionNew servlet

--- src/main/java/servlets/auction/AuctionNew.java
+++ src/main/java/servlets/auction/AuctionNew.java
@@ -4,8 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import main.java.dao.AuctionDAO;
 import main.java.dao.DAOException;
+import main.java.dao.EventDAO;
+import main.java.dao.UserDAO;
 import main.java.dao.sql.AuctionDAOSQL;
+import main.java.dao.sql.EventDAOSQL;
+import main.java.dao.sql.UserDAOSQL;
 import main.java.models.Auction;
+import main.java.models.Event;
+import main.java.models.User;
 import main.java.utils.HttpRequest;
 import main.java.utils.HttpSession;
 import main.java.utils.JsonResponse;
@@ -27,11 +33,15 @@ public class AuctionNew extends HttpServlet {
     public static final String AUCTION_START_TIME_ERROR = "Auction must have a start time";
     public static final String AUCTION_STATUS_ERROR = "Status must be valid";
     public static final String AUCTION_IS_NOT_VALID_ERROR = "Auction is not valid";
+    public static final String OWNER_NOT_EXIST_ERROR = "Owner does not exist";
+    public static final String EVENT_NOT_EXIST_ERROR = "Event does not exist";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         JsonResponse jsonResponse = new JsonResponse(response);
         HttpSession session = new HttpSession(request);
         AuctionDAO auctionDAO = AuctionDAOSQL.getInstance();
+        UserDAO userDAO = UserDAOSQL.getInstance();
+        EventDAO eventDAO = EventDAOSQL.getInstance();
 
         int userId = session.userId();
         if (userId == -1) {
@@ -65,6 +75,32 @@ public class AuctionNew extends HttpServlet {
             return;
         }
 
+        User owner;
+        try {
+            owner = userDAO.getById(unsafeAuction.ownerId);
+        } catch (DAOException e) {
+            Logger.error("Get user by ID " + unsafeAuction.ownerId, e.toString());
+            jsonResponse.internalServerError();
+            return;
+        }
+        if (owner == null) {
+            jsonResponse.error(OWNER_NOT_EXIST_ERROR);
+            return;
+        }
+
+        Event event;
+        try {
+            event = eventDAO.getById(unsafeAuction.eventId);
+        } catch (DAOException e) {
+            Logger.error("Get event by ID " + unsafeAuction.eventId, e.toString());
+            jsonResponse.internalServerError();
+            return;
+        }
+        if (event == null) {
+            jsonResponse.error(OWNER_NOT_EXIST_ERROR);
+            return;
+        }
+
         Auction newAuction = new Auction();
         newAuction.name = unsafeAuction.name;
         newAuction.startingPrice = unsafeAuction.startingPrice;
@@ -72,7 +108,7 @@ public class AuctionNew extends HttpServlet {
         newAuction.eventId = unsafeAuction.eventId;
         newAuction.ownerId = userId;
         newAuction.status = unsafeAuction.status;
-        newAuction.winnerId = unsafeAuction.winnerId;
+        newAuction.winnerId = 0;
         newAuction.isValid = unsafeAuction.isValid;
 
         Auction dbAuction;
