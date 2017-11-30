31612d4f82dffdc14cc8e92d1fc6afc58ad8fb0d
zurfyx <zurfyx@gmail.com>
2017-11-30 16:53:16 +0100

    fix user deauthenticate should extend from servlet

--- src/main/java/servlets/user/UserDeauthenticate.java
+++ src/main/java/servlets/user/UserDeauthenticate.java
@@ -5,12 +5,13 @@ import main.java.utils.JsonResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "UserDeauthenticate", urlPatterns = {"/users/deauthenticate" })
-public class UserDeauthenticate {
+public class UserDeauthenticate extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = new HttpSession(request);
         session.save(HttpSession.USER_ID_KEY, -1);
