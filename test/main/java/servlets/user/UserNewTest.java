f800e57c32ce8f1eb3fad7000ab73cd1cfb3370a
zurfyx <zurfyx@gmail.com>
2017-12-03 12:55:34 +0100

    authenticate after sign up

--- test/main/java/servlets/user/UserNewTest.java
+++ test/main/java/servlets/user/UserNewTest.java
@@ -5,6 +5,7 @@ import main.java.dao.sql.AbstractDBTest;
 import main.java.mocks.HttpServletStubber;
 import main.java.utils.DummyGenerator;
 import main.java.models.User;
+import main.java.utils.HttpSession;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -29,4 +30,16 @@ public class UserNewTest extends AbstractDBTest {
         assertNotNull(outputUser.updatedAt);
     }
 
+    @Test
+    public void should_authenticate_after_creating_user() throws Exception {
+        User dummyUser = DummyGenerator.getDummyUser();
+        String dummyUserJson = new Gson().toJson(dummyUser);
+
+        HttpServletStubber stubber = new HttpServletStubber();
+        stubber.body(dummyUserJson).listen();
+        new UserNew().doPost(stubber.servletRequest, stubber.servletResponse);
+        User outputUser = new Gson().fromJson(stubber.gathered(), User.class);
+
+        assertEquals(outputUser.id, stubber.authenticated());
+    }
 }
